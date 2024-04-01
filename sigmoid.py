c8378385c59e454677d3bc7fcc4d141f3e65ee49
kumar-332 <kumar.332@iitj.ac.in>
2024-04-02 00:47:51 +0530

    feature-1

---
+++ sigmoid.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+random_values = [-3.5, -1.2, 0, 2.8, -4.1, 1.5, -0.7, 3.2, -2.4, 4.6]
+sigmoid_output = 1 / (1 + np.exp(-np.array(random_values)))
+
+print("Sigmoid output:", sigmoid_output)
